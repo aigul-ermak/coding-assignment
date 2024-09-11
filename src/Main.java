e7daefc433fda3acecd3a85b29b6e2deee83aab4
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-11 17:04:37 -0300

    Added logic for AirConditioner, added const for messages

--- src/Main.java
+++ src/Main.java
@@ -26,11 +26,45 @@ class Light {
     }
 }
 
+class AirConditioner {
+    private ApplianceState state;
+    private String mode;
+
+    public AirConditioner() {
+        this.state = ApplianceState.OFF;
+        this.mode = "off";  // Default mode is "off"
+    }
+
+    public void toggle() {
+        if (this.state == ApplianceState.OFF) {
+            this.state = ApplianceState.ON;
+            this.mode = "cooling";  // When turning on, set mode to "cooling"
+            System.out.println("The air conditioner is now ON in cooling mode.");
+        } else {
+            this.state = ApplianceState.OFF;
+            this.mode = "off";  // When turning off, set mode to "off"
+            System.out.println("The air conditioner is now OFF (thermostat set to 'off' mode).");
+        }
+    }
+
+    public ApplianceState getState() {
+        return this.state;
+    }
+
+    public String getMode() {
+        return this.mode;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         Light light = new Light();
+        AirConditioner ac = new AirConditioner();
+
+        final String INVALID_CHOICE_MESSAGE = "Invalid choice. Please enter y, n, or q.";
+        final String EXIT_MESSAGE = "Exiting the system. Goodbye!";
 
         System.out.println("Hi! You are in the Smart Home Appliance Control System!");
 
@@ -42,7 +76,7 @@ public class Main {
             String choice = sc.nextLine().toUpperCase();
 
             if (choice.equals("Q")) {
-                System.out.println("Exiting the system. Bye!");
+                System.out.println(EXIT_MESSAGE);
                 break;
             }
 
@@ -57,10 +91,10 @@ public class Main {
                         } else if (toggleChoice.equals("n")) {
                             break;
                         } else if (toggleChoice.equals("q")) {
-                            System.out.println("Exiting the system. Bye!");
+                            System.out.println(EXIT_MESSAGE);
                             System.exit(0);
                         } else {
-                            System.out.println("Invalid choice. Please enter y, n, or q.");
+                            System.out.println(INVALID_CHOICE_MESSAGE);
                         }
                     }
                     break;
@@ -68,7 +102,21 @@ public class Main {
                     System.out.println("You selected Fan.");
                     break;
                 case "A":
-                    System.out.println("You selected Air Conditioner.");
+                    while (true) {
+                        System.out.println("You selected Air Conditioner. The current state is: " + ac.getState());
+                        System.out.println("Would you like to toggle the air conditioner? (y for yes, n for no, q to quit)");
+                        String toggleChoice = sc.nextLine().toLowerCase();
+                        if (toggleChoice.equals("y")) {
+                            ac.toggle();
+                        } else if (toggleChoice.equals("n")) {
+                            break;  //
+                        } else if (toggleChoice.equals("q")) {
+                            System.out.println(EXIT_MESSAGE);
+                            System.exit(0);
+                        } else {
+                            System.out.println(INVALID_CHOICE_MESSAGE);
+                        }
+                    }
                     break;
                 default:
                     System.out.println("Invalid selection. Please enter L, F, or A.");
